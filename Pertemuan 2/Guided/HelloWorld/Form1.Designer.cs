4bb81e17aecb48ee106ca13de25e6ac2e90afcc8
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2025-03-05 05:20:51 +0700

    "creating console app and form" pertemuan 2

---
+++ Pertemuan 2/Guided/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PressButton = new Button();
+            SuspendLayout();
+            // 
+            // PressButton
+            // 
+            PressButton.Location = new Point(350, 190);
+            PressButton.Name = "PressButton";
+            PressButton.Size = new Size(94, 29);
+            PressButton.TabIndex = 0;
+            PressButton.Text = "Press";
+            PressButton.UseVisualStyleBackColor = true;
+            PressButton.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(PressButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button PressButton;
+    }
+}
