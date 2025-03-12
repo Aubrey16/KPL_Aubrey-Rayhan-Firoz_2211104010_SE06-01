514042c4fca749fef7719b6bed92407d252dfc66
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2025-03-13 06:24:38 +0700

    Project Tp

---
+++ Pertemuan 3/tpModul5_2211104010/tpModul5_2211104010/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Aubrey"); 
+
+
+        DataGeneric<string> data = new DataGeneric<string>("2211104010");
+        data.PrintData();
+    }
+}
