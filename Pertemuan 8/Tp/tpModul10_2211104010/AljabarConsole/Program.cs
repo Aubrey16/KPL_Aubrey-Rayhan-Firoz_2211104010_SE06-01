ff9f60d521770aec28945df7e7f2af24b1ab5044
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2025-05-24 22:01:25 +0700

    TP10

---
+++ Pertemuan 8/Tp/tpModul10_2211104010/AljabarConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace AljabarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] persamaanKuadrat = { 1, -3, -10 };
+            double[] akar = Aljabar.AkarPersamaanKuadrat(persamaanKuadrat);
+            Console.WriteLine($"Akar-akar persamaan: {akar[0]}, {akar[1]}");
+
+            double[] persamaanLinear = { 2, -3 };
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(persamaanLinear);
+            Console.WriteLine($"Hasil kuadrat: {hasilKuadrat[0]}x^2 + {hasilKuadrat[1]}x + {hasilKuadrat[2]}");
+        }
+    }
+}
---
+++ Pertemuan 8/Tp/tpModul10_2211104010/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                throw new Exception("Tidak memiliki akar real.");
+            }
+
+            double sqrtDisc = Math.Sqrt(discriminant);
+            double x1 = (-b + sqrtDisc) / (2 * a);
+            double x2 = (-b - sqrtDisc) / (2 * a);
+
+            return new double[] { x1, x2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double hasilA = a * a;
+            double hasilB = 2 * a * b;
+            double hasilC = b * b;
+
+            return new double[] { hasilA, hasilB, hasilC };
+        }
+    }
+}
