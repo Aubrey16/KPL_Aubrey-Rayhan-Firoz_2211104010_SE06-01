ede0444e1be281fc1d3cb5207f89827be22f67bf
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2025-05-26 00:26:48 +0700

    Tp14, refactored Tp 5

---
+++ Pertemuan_12/Tp/tpModul14_2211104010/tpModul5_2211104010/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TpModul14
+{
+    /// <summary>
+    /// Kelas untuk menyapa user dengan generic.
+    /// </summary>
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+
+    /// <summary>
+    /// Kelas generic untuk menyimpan dan menampilkan data.
+    /// </summary>
+    /// <typeparam name="T">Tipe data generic.</typeparam>
+    public class DataGeneric<T>
+    {
+        private T _data;
+
+        public DataGeneric(T data)
+        {
+            _data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {_data}");
+        }
+    }
+
+    /// <summary>
+    /// Kelas utama untuk menjalankan program.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main()
+        {
+            var halo = new HaloGeneric();
+            halo.SapaUser("Aubrey");
+
+            var data = new DataGeneric<string>("2211104010");
+            data.PrintData();
+        }
+    }
+}
